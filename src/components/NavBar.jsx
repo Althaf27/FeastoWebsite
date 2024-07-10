f37f263f3bc4dac6fcb020d429b04749bc951e90
Althaf Ahamed <althafahamed282@gmail.com>
2024-07-11 01:24:54 +0530

    implementing nav-bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,15 @@ import logoFeato from "../assets/images/logofeasto.png";
 export default function NavBar() {
   return (
     <section>
-      <div>
-        <img
-          src={logoFeato}
-          alt="feasto logo"
-          width={110}
-          className="object-contain"
-        />
+      <div className="flex px-[16px] h-[80px] border justify-between items-center">
+        <div>
+          <img
+            src={logoFeato}
+            alt="feasto logo"
+            width={110}
+            className="object-contain"
+          />
+        </div>
       </div>
     </section>
   );
